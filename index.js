c6ef89b0580c6f96d52e5342675897dad1420bb4
lucia-garzon <lucgarzon.uni@gmail.com>
2024-02-12 13:00:10 -0500

    📝 Create index.js

---
+++ index.js
@@ -0,0 +1,18 @@
+// const birthday;       
+// const birthday_month; 
+// const birthday_day;   
+// let selected_sign;    
+
+
+// const signs = [
+//     {
+//       name: 'Gemini',
+//       image: 'twins.jpg'
+//       sound: 'sparkle.mp3'
+//     }
+//   ]
+
+
+function play_sound(selected_sign) {
+    // play a sound based on selected_sign
+  }
