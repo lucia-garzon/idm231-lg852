d59755e5dee84b63bae65f4ec2b699bdd2d41dce
lucia-garzon <lucgarzon.uni@gmail.com>
2024-03-02 12:42:49 -0500

    📝 Add form code

--- js/index.js
+++ js/index.js
@@ -16,3 +16,31 @@
 // function play_sound(selected_sign) {
 //   }
 
+const form = document.querySelector(".birthday-form"); // <form> element
+
+function handle_submit(event) {
+    event.preventDefault(); // Prevent the default form submission
+    console.group("Form submission");
+    console.log(form); // <form> element
+    console.log(form.elements); // array of form elements
+    console.log(form.elements["birthday"].value); // 1990-01-01
+    console.groupEnd();
+
+    const birthdayInputValue = form.elements["birthday"].value;
+
+    console.log(birthdayInputValue); // 2024-02-20
+    const date = birthdayInputValue.split("-"); // ["2024", "02", "20"]
+    const year = date[0]; // 2024
+    const day = date[2]; // 20
+    const month = date[1]; // 02
+
+    console.log('Year:', year, 'Month:', month, 'Day:', day);
+
+    const errors = [];
+    if (birthdayInputValue === "") {
+        errors.push("Birthdate is required");
+    }
+    console.log(errors); 
+}
+
+form.addEventListener("submit", handle_submit);
