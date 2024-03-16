7df4c5a71334051d834b02a809d7d6395d29003b
lucia-garzon <lucgarzon.uni@gmail.com>
2024-03-16 19:16:26 -0400

    Delete comments

--- js/index.js
+++ js/index.js
@@ -1,20 +1,3 @@
-// const birthday;       
-// const birthday_month; 
-// const birthday_day;   
-// let selected_sign;    
-
-
-// const signs = [
-//     {
-//       name: 'Gemini',
-//       image: 'twins.jpg'
-//       sound: 'sparkle.mp3'
-//     }
-//   ]
-
-
-// function play_sound(selected_sign) {
-//   }
 
 
 // birthday form  birthday form   birthday form   birthday form   birthday form   birthday form   birthday form   birthday form   birthday form   
